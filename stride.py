d6fe3f71b85aa73ad2808f106b738f2e1e794764
A.J. Bate <ajb300@cam.ac.uk>
2013-10-09 14:37:15 +0100

    Stride wrapper functionally complete

--- stride.py
+++ stride.py
@@ -1,7 +1,14 @@
-import sys, os
+import sys, os, time
 sys.path.insert(0, os.path.join(os.path.dirname(__file__),'libs'))
 import requests
 import xml.etree.ElementTree as ET
+
+def output_text(msg):
+	for char in msg:
+		sys.stdout.write ( '%s' % char)
+		sys.stdout.flush()
+		time.sleep(0.05)
+	print ''
 
 def fetchURL(lat,lon,radius):
 	payload = {'near': str(lat) + ',' +  str(lon) + ',' +  str(radius)}
@@ -9,17 +16,27 @@ def fetchURL(lat,lon,radius):
 	r = requests.get(requesturl, params=payload, auth=('7b399134-f740-4432-9a3e-f6d711473558', ''))
 	return r
 
-data = fetchURL(52.211604,0.09166,1000000)
-parsedXML = ET.fromstring(data.text)
-#for child in parsedXML.iter('description'):
-#	print child.text
-events=[]
-for x in range(10):
-	try:
-		descrip =  parsedXML[x][5][5].text
-		urgency = parsedXML[x][5][2].text
-		severity = parsedXML[x][5][3].text
-		certainty = parsedXML[x][5][4].text
-		events[x] = {'descrip': descrip, 'urgency' : urgency, 'severity': severity, 'certainty':certainty}
-	except IndexError:
-		pass
+def parseData(lon, lat, radius):
+	data = fetchURL(lon, lat, radius)
+	parsedXML = ET.fromstring(data.text)
+	events=[]
+	for x in range(10):
+		try:
+			descrip =  parsedXML[x][5][5].text
+			urgency = parsedXML[x][5][2].text
+			severity = parsedXML[x][5][3].text
+			certainty = parsedXML[x][5][4].text
+			latlong = parsedXML[x][5][9][0].text
+			road = parsedXML[x][5][9][5].text
+			direction = parsedXML[x][5][9][4].text
+			intersection = parsedXML[x][5][9][1].text
+			events.append({'descrip': descrip, 'urgency' : urgency, 'severity': severity, 'certainty':certainty, 'latlong' : latlong, 'road': road, 'direction': direction, 'intersection' : intersection})
+		except IndexError:
+			pass
+		return events
+
+currentEvents = parseData(52.211604, 0.09166, 10000)
+
+for event in currentEvents:
+	message = 'There is a ' + event['severity'] + ' problem on the ' + event['road'] +  ' ' + event['direction'] + ' near ' + event['intersection'] + '.\n' + event['descrip']
+	output_text(message)
