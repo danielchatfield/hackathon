b586ae2f686171cea646dcc50951515c9b1545ae
Daniel Chatfield <chatfielddaniel@gmail.com>
2013-10-09 17:27:52 +0100

    blasd

--- frontend/assets/route.js
+++ frontend/assets/route.js
@@ -412,6 +412,7 @@ function getPolylineXml() {
 }
 
 function getIssues() {
+  $('body').css('opacity',0.1);
   directionsResponse = directionsDisplay.getDirections();
   if (!directionsResponse) { alert("no route"); return; }
 
@@ -424,8 +425,64 @@ function getIssues() {
   }
   var data = JSON.stringify(points);
   console.log(data);
-  $.post('/locationAPI', data, function(){
+  var success = function(data){
+    $('body').css('opacity',1);
+    data = jQuery.parseJSON(data);
+    console.log(data);
+    var limit = data.length;
+    var openWindow = function(infoWindow) {
+      return function() {
+        infowindow.open(map,marker);
+      }
+    }
+    for(var i = 0; i < limit; i++) {
+      // add marker
+      var issue = data[i];
+      console.log(issue['latlong'])
+      var coords = issue['latlong'].replace('POINT(', '').replace(')', '').split(' ');
+      console.log(coords);
+      if(coords.length > 2) {
+        continue;
+      }
+      var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+      var myLatlng = new google.maps.LatLng(coords[1], coords[0]);
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: issue['descrip']
+      });
+      google.maps.event.addListener(marker, 'click', openWindow(marker));
+    }
+  };
+  $.ajax('/locationAPI',{
+    'data': data, 
+    'type': 'POST',
+    'processData': false,
+    'contentType': 'application/json',
+    'success': success
   });
 }
 
