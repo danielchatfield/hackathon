2e35abcec74d5619e90ec42d2f2c411ad54db0c8
A.J. Bate <ajb300@cam.ac.uk>
2013-10-09 13:10:18 +0100

    Basic functionality

--- stride.py
+++ stride.py
@@ -1,18 +1,16 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__),'libs'))
 import requests
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 
-def createURL(lat,lon,radius):
-	payload = {'near': str(lat) + ' ' +  str(lon) + ',' +  str(radius)
-	requesturl='http://api.stride-project.com/events/feeds/d38a9c31-3156-446e-a89e-79aa3d7357c2/datastreams/1/events.json'
-	requesturl = requesturl + '?near=' + str(lat) + ',' + str(lon) + ',' + str(radius)
-	return requesturl
+def fetchURL(lat,lon,radius):
+	payload = {'near': str(lat) + ',' +  str(lon) + ',' +  str(radius)}
+	requesturl='http://api.stride-project.com/events/feeds/d38a9c31-3156-446e-a89e-79aa3d7357c2/datastreams/1/events'
+	r = requests.get(requesturl, params=payload, auth=('7b399134-f740-4432-9a3e-f6d711473558', ''))
+	return r
 
-URL = createURL(52.211604,0.09166,1000000)
-
-
-r = requests.get(URL, auth=('7b399134-f740-4432-9a3e-f6d711473558', ''))
-
-print r.text
+data = fetchURL(52.211604,0.09166,1000)
+parsedXML = ET.fromstring(data.text)
+for child in parsedXML.iter('description'):
+	print child.text
 
